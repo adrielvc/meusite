903489fecfa5e8427e6f49b3f0ca44d7a77f7527
Priscila Cardoso <priscila.cardoso@gmail.com>
2020-03-04 21:41:26 -0300

    commitando template

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return '<h1>Meu primeiro site no Flask localmente</h1>'
+
+@app.route('/inicio')
+def inicio():
+    return render_template('Index.html')
+
 
